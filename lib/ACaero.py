9bfe2a3a9292a0955c2b2d31821a5351126b2861
RenjaminBupe <benrupe@gmail.com>
2023-11-12 14:12:23 -0500

    Remove propeller torque

--- lib/ACaero.py
+++ lib/ACaero.py
@@ -44,7 +44,7 @@ class Aero():
                                                        [b*(C_n_0 + C_n_beta*beta + C_n_p*(b/(2*Va))*p + C_n_r*(b/(2*Va))*r +\
                                                             C_n_delta_a*d_a + C_n_delta_r*d_r)]])
         
-        propeller = np.array([[-k_Tp*((k_omega*d_t)**2)],
+        propeller = np.array([[0],
                               [0],
                               [0]])
         
