b04ccf0edd740445a84d54969bc5bbbc50ab7c9f
sladesladeslade <spbrooks4@gmail.com>
2023-11-12 18:16:31 -0500

    adding AP stuff

---
+++ autopilotTest.py
@@ -0,0 +1,58 @@
+# Autopilot Testing File
+# Everyone
+# lets test this boy
+
+import sys
+import os
+cwd = os.getcwd()
+sys.path.append(cwd)
+import numpy as np
+import keyboard
+import lib.ACanim as anim
+import lib.ACdynamics as acd
+import lib.ACaero as aca
+import lib.carrier_dynamics as car
+import lib.wind as wind
+from obj.hangar import *
+from lib.simparams import *
+
+
+###### Initialize Misc Classes ######
+anim = anim.animation(100, 0.4)
+ac_dyn = acd.ACdynamics()
+ac_aero = aca.Aero()
+car_dyn = car.carrier_dynamics(0.)
+Vsteady = np.array([[0.], [0.], [0.]])
+wind = wind.wind(Vsteady)
+
+## Initalize Sim Time ##
+t = start_time
+Va = 35.
+
+## Main Sim Loop ##
+while t < end_time:
+    t_next_plot = t + ts_plotting
+    while t < t_next_plot:
+        # update carrier dynamics
+        car_dyn.update(t, False)
+        
+        # do wind
+        Va, alpha, beta = wind.wind_char(ac_dyn.state, Va, ts_simulation)
+        
+        # autopilot
+        
+        # aero
+        
+        # dynamics
+
+        # anim update
+        anim.update(f18_verts, carrier_verts, ac_dyn.state, car_dyn.state, ["b"], ["g"])
+        
+        # iterate time
+        t += ts_simulation
+        
+    # do plotting
+    
+    # check for keybaord press
+    plt.pause(0.01)
+    if keyboard.is_pressed('q'): break
